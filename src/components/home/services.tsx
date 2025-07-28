d55e2fad26dcc25080ef7ec0381b45b304873723
Christine Nyambwari <christinenyambwari@gmail.com>
2025-07-28 11:03:35 -0700

    commit

--- src/components/home/services.tsx
+++ src/components/home/services.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Your Go backend URL
+  baseURL: 'http://localhost:3000/api', 
   headers: {
     'Content-Type': 'application/json',
   },
