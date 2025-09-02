c7a63d7886732c0189255f7b48be845ef4f4e4cf
Christine Nyambwari <christinenyambwari@gmail.com>
2025-09-02 01:58:27 -0700

    siteMap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import ViteSitemap from 'vite-plugin-sitemap';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteSitemap({
+      hostname: 'https://quisells.com',
+    })
+  ],
   server: {
   port: 5173,
   proxy: {
