44ce7aaaa339a7d3677a5e271a0e0918166909c3
Christine Nyambwari <christinenyambwari@gmail.com>
2025-10-24 18:25:07 +0300

    commit

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -131,7 +131,7 @@ export function Navbar({ children }: NavbarProps) {
 
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
-      <div className="container mx-auto px-4 py-3 sm:py-4">
+      <div className="container mx-auto px-4 py-2 sm:py-3">
         <div className="flex items-center justify-between">
       
 
